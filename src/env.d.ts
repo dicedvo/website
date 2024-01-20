819bfcfe5e2b614a73b722113f177677d107ee7a
Ned Palacios <npdoesmc@gmail.com>
2024-01-20 12:52:45 +0800

    fix: select env to expose in the public

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,12 +5,12 @@ interface ImportMetaEnv {
     readonly CONTENTFUL_SPACE_ID: string;
     readonly CONTENTFUL_DELIVERY_TOKEN: string;
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
-    readonly FACEBOOK_PAGE_ID: string;
-    readonly FIREBASE_API_KEY: string;
-    readonly FIREBASE_AUTH_DOMAIN: string;
-    readonly FIREBASE_PROJECT_ID: string;
-    readonly FIREBASE_STORAGE_BUCKET: string;
-    readonly FIREBASE_MESSAGING_SENDER_ID: string;
-    readonly FIREBASE_APP_ID: string;
-    readonly FIREBASE_MEASUREMENT_ID: string;
+    readonly PUBLIC_FACEBOOK_PAGE_ID: string;
+    readonly PUBLIC_FIREBASE_API_KEY: string;
+    readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    readonly PUBLIC_FIREBASE_PROJECT_ID: string;
+    readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    readonly PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly PUBLIC_FIREBASE_APP_ID: string;
+    readonly PUBLIC_FIREBASE_MEASUREMENT_ID: string;
   }
--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  measurementId: import.meta.env.PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
