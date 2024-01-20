7db28cd819cb7478374c714f64c862c5fd35f66d
Ned Palacios <npdoesmc@gmail.com>
2024-01-20 12:27:31 +0800

    feat: add fb messenger chat bot

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,5 @@ interface ImportMetaEnv {
     readonly CONTENTFUL_SPACE_ID: string;
     readonly CONTENTFUL_DELIVERY_TOKEN: string;
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
+    readonly FACEBOOK_PAGE_ID: string;
   }
-  
