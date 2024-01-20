6b41e341d5759021bcdb6596d75b8e398e8db83c
Ned Palacios <npdoesmc@gmail.com>
2024-01-20 12:51:47 +0800

    feat: firebase support

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,4 +6,11 @@ interface ImportMetaEnv {
     readonly CONTENTFUL_DELIVERY_TOKEN: string;
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
     readonly FACEBOOK_PAGE_ID: string;
+    readonly FIREBASE_API_KEY: string;
+    readonly FIREBASE_AUTH_DOMAIN: string;
+    readonly FIREBASE_PROJECT_ID: string;
+    readonly FIREBASE_STORAGE_BUCKET: string;
+    readonly FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly FIREBASE_APP_ID: string;
+    readonly FIREBASE_MEASUREMENT_ID: string;
   }
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+
+// Analytics
+export const analytics = getAnalytics(app);
